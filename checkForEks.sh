eb078fb2434873b08296ef828ef615b259b3fecc
Connor Smith <jindego@live.com>
2021-03-07 16:39:08 +0000

    check region var

--- checkForEks.sh
+++ checkForEks.sh
@@ -1,9 +1,9 @@
 checkForEKS=$(grep -i eksctl-UdacityCapstone-cluster list.txt | wc -l)
+echo $AWS_DEFAULT_REGION
 if [[ $checkForEKS -gt 0 ]]
     then 
     echo "we have $checkForEKS results, Skipping cluster creation"
     else
     echo "Cant find kube cluster, creating..."
-    echo $AWS_DEFAULT_REGION
     #eksctl create cluster --name UdacityCapstone --version 1.19 --region $AWS_DEFAULT_REGION --nodegroup-name UdacityCapstone --node-type t3.nano --nodes-min 2 --nodes-max 4 --managed
 fi
