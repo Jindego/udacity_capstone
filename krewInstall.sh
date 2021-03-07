db26dc3ab2372afea01c0b682ef5312f951d7b49
Connor Smith <jindego@live.com>
2021-03-07 17:13:47 +0000

    test

--- krewInstall.sh
+++ krewInstall.sh
@@ -3,3 +3,4 @@
   tar zxvf krew.tar.gz &&
   KREW=./krew-"$(uname | tr '[:upper:]' '[:lower:]')_$(uname -m | sed -e 's/x86_64/amd64/' -e 's/arm.*$/arm/')" &&
   "$KREW" install krew
+  export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"
