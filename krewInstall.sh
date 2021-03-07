a7c14edea7bf6aa874d5d1c966b98f870aa4c7be
Connor Smith <jindego@live.com>
2021-03-07 17:19:43 +0000

    tst

--- krewInstall.sh
+++ krewInstall.sh
@@ -4,4 +4,3 @@
   KREW=./krew-"$(uname | tr '[:upper:]' '[:lower:]')_$(uname -m | sed -e 's/x86_64/amd64/' -e 's/arm.*$/arm/')" &&
   "$KREW" install krew
   export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"
-  exec bash
